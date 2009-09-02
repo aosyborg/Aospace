2598c61aaf448667a8fd6d438b855b8b4d1af902
aozaki <symons@aospace.com>
2009-09-02 00:12:15 -0700

    Working state

--- controllers/ResumeController.php
+++ controllers/ResumeController.php
@@ -6,7 +6,7 @@ class ResumeController extends Aospace_Controller_Base
 {
 	public function indexAction()
 	{
-		$this->view->header->setTitle('About')
+		$this->view->header->setTitle('Résumé')
 		                   ->addLinkTag('rel="stylesheet" type="text/css" href="/css/resume.css"');
 	}
 }
